aa6c5590dc36c6274a58960a79982272d5714c5a
Dinis239 <168186503+Dinis239@users.noreply.github.com>
2025-12-01 11:13:25 +0000

    Clustering done (SOM unfinished)

--- Clustering/utils_clustering.py
+++ Clustering/utils_clustering.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import math
+import pandas as pd
 
 
 def visualize_dimensionality_reduction(transformation: np.array,
@@ -26,4 +28,39 @@ def visualize_dimensionality_reduction(transformation: np.array,
                          edgecolor=None,
                          palette='tab10')
     sns.move_legend(ax, loc='upper left', bbox_to_anchor=(1, 1))
+    plt.show()
+
+
+def boxplot_grid(data: pd.DataFrame, 
+                 variables: list,
+                 cluster_var: str,
+                 color: str = None) -> None:
+    """
+    Plot a boxploty grid based on the data.
+
+    Parameters:
+        ----------
+         - data (pd.DataFrame): The DataFrame containing the data.
+         - variables (list): The column names of the variables to be plotted.
+         - color (str, optional): Color for the bars. Defaults to None.
+         - edgecolor (str, optional): Color for the bars edges.
+         Defaults to 'black'.
+
+    Returns:
+        ----------
+         None, but a plot is produced
+    """
+    a = math.ceil(len(variables)/3)
+    b = 3
+    fig, axes = plt.subplots(a, b, figsize=(25, 75))
+    axes = axes.flatten()
+    for i, column in enumerate(variables):
+        sns.boxplot(x=data[cluster_var], y=column,
+                    data=data, ax=axes[i], color=color)
+        axes[i].set_xticklabels(axes[i].get_xticklabels(), rotation=45)
+        axes[i].set_title(column)
+    axes_to_turn_off = (a * b) - len(variables)
+    for i in range(1, axes_to_turn_off + 1):
+        axes[-i].axis('off')
+    plt.tight_layout()
     plt.show()
